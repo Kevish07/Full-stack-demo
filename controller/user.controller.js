c1f1204c57b7f1f01fb51fdc0bd0027a0e9a472c
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-05-09 15:02:30 +0530

    user validation by DB

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,3 +1,52 @@
+import User from "../model/User.model.js"
+import crypto from "crypto"
+
 export const registerUser = async (req,res)=>{
-    res.send("User Registered Successfully")
+    // res.send("User Registered Successfully")
+
+    const {name,email,password} = req.body
+    if (!name || !email || !password){
+        return res.status(400).json({
+            message:"All fields are required"
+        })
+    }
+    console.log(email)
+
+    try {
+
+        // User Validation
+        const existingUser = await User.findOne({email})
+        if (existingUser) {
+            return res.status(400).json({
+                message:"User already exists"
+            })
+        }
+
+        const user = User.create({
+            name,
+            email,
+            password
+        })
+        console.log(user)
+
+        if (!user){
+            return res.status(400).json({
+                message:"User not registered"
+             })
+        }
+
+        const token = crypto.randomBytes(32).toString("hex")
+        console.log(token)
+        user.verificationToken = token
+
+        await user.save()
+
+
+        // Sending Emails
+
+        
+
+    } catch (error) {
+        
+    }
 }
