f2143c673611a0ac17d9651510794e7b109f0e5a
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-05-09 16:08:02 +0530

    token not added to DB

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,82 +1,87 @@
-import User from "../model/User.model.js"
-import crypto from "crypto"
-import nodemailer from "nodemailer"
-import { join } from "path"
+import User from "../model/User.model.js";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
 
-export const registerUser = async (req,res)=>{
-    // res.send("User Registered Successfully")
+export const registerUser = async (req, res) => {
+  // res.send("User Registered Successfully")
 
-    const {name,email,password} = req.body
-    if (!name || !email || !password){
-        return res.status(400).json({
-            message:"All fields are required"
-        })
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+  console.log(email);
+
+  try {
+    // User Validation
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
     }
-    console.log(email)
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    console.log(user);
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not registered",
+      });
+    }
+
+    // Token Generation
+
+    const token = crypto.randomBytes(32).toString("hex");
+    console.log(token);
+    user.verificationToken = token;
 
     try {
-
-        // User Validation
-        const existingUser = await User.findOne({email})
-        if (existingUser) {
-            return res.status(400).json({
-                message:"User already exists"
-            })
-        }
-
-        const user = User.create({
-            name,
-            email,
-            password
-        })
-        console.log(user)
-
-        if (!user){
-            return res.status(400).json({
-                message:"User not registered"
-             })
-        }
-
-        const token = crypto.randomBytes(32).toString("hex")
-        console.log(token)
-        user.verificationToken = token
-
-        await user.save()
-
-        // Sending Emails
-
-        const transporter = nodemailer.createTransport({
-            host: process.env.MAILTRAP_HOST,
-            port: process.env.MAILTRAP_PORT,
-            secure: false, // true for 465, false for other ports
-            auth: {
-              user: process.env.MAILTRAP_USERNAME,
-              pass: process.env.MAILTRAP_PASSWORD,
-            },
-          });
-
-
-        const mailOptions = {
-            from: process.env.MAILTRAP_SENDEREMAIL,
-            to: user.email,
-            subject: "Verify your email",
-            text: `To verify click on the link below given
-            ${process.env.BASE_URL}/api/v1/user/verify${token}`,
-        }
-
-        await transporter.sendMail(mailOptions)
-
-        res.status(201).json({
-            message:"User registered Successfully",
-            success: true
-        })
-
+      await user.save();
+      console.log("User saved with token");
     } catch (error) {
-        res.status(400).json({
-            message:"fail to register user, error occurred",
-            error,
-            success: false
-        })
-
+      console.error("Error saving user:", err.message);
+      return res
+        .status(500)
+        .json({ message: "Failed to save user", error: err.message });
     }
-}
+
+    // Sending Emails
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST,
+      port: process.env.MAILTRAP_PORT,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.MAILTRAP_USERNAME,
+        pass: process.env.MAILTRAP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.MAILTRAP_SENDEREMAIL,
+      to: user.email,
+      subject: "Verify your email",
+      text: `To verify click on the link below given
+            ${process.env.BASE_URL}/api/v1/user/verify${token}`,
+    };
+
+    transporter.sendMail(mailOptions);
+
+    res.status(201).json({
+      message: "User registered Successfully",
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "fail to register user, error occurred",
+      error,
+      success: false,
+    });
+  }
+};
--- model/User.model.js
+++ model/User.model.js
@@ -1,33 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: String,
     email: String,
     password: String,
     role: {
-        type:String,
-        enum:["user","admin"],
-        default:"user",
-        isVerified:{
-            type: Boolean,
-            default: false,
-        },
-        verificationToken:{
-            type: String,
-        },
-        resetPasswordToken: {
-            type: String,
-        }
-        ,resetPasswordExpires: {
-            type: Date,   
-        },
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+      isVerified: {
+        type: Boolean,
+        default: false,
+      },
+      verificationToken: {
+        type: String,
+      },
+      resetPasswordToken: {
+        type: String,
+      },
+      resetPasswordExpires: {
+        type: Date,
+      },
     },
-},
-{
-    timestamps: true
-}
-)
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model("User",userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
